9cc68602f4ff1440ffd0940e2fe1625c9b1f80b6
ZeebotX <MLLZUH001@myuct.ac.za>
2026-03-19 11:28:44 +0200

    Initial Commit

---
+++ RazorPagesMovie/Migrations/20260305113352_rrrrrr.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class rrrrrr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+        }
+    }
+}
---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,65 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new RazorPagesMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<RazorPagesMovieContext>>()))
+        {
+            if (context == null || context.Movie == null)
+            {
+                throw new ArgumentNullException("Null RazorPagesMovieContext");
+            }
+
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "Star Wars Ep. VII: The Force Awakens",
+                    ReleaseDate = DateTime.Parse("2015-12-16"),
+                    Genre = "Science Fiction",
+                    Price = 533.2M,
+                    Rating = "R"
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M,
+                    Rating = "R"
+                },
+
+                new Movie
+                {
+                    Title = "Avatar: The Way of Water",
+                    ReleaseDate = DateTime.Parse("2022-12-09"),
+                    Genre = "Comedy",
+                    Price = 9.99M,
+                    Rating = "R"
+                },
+
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M,
+                    Rating = "R"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
